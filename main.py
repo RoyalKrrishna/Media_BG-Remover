f19ba17494d35804e10e6ec596bd99ccea01be3f
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-06-14 15:33:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,171 +1,318 @@
-#Made with Python3
-#(C) Rexinazor
-#Copyright permission under MIT License
-#License -> https://github.com/Rexinazor/Media_BG-Remover/blob/main/LICENSE
+# (c) @RoyalKrrishna
 
-import os
-import requests
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from configs import Config
+from pyrogram import Client, filters, idle
+from pyrogram.errors import QueryIdInvalid
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InlineQuery, InlineQueryResultArticle, \
+    InputTextMessageContent
+from TeamTeleRoid.forcesub import ForceSub
+import asyncio
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon import Button
+from tmdbv3api import TMDb, Movie, TV
+from telethon.errors import UserNotParticipantError
+from telethon.tl.functions.channels import GetParticipantRequest
 
-API = os.environ["REMOVEBG_API"]
-IMG_PATH = "./DOWNLOADS"
-
-Rexinazor = Client(
-    "Remove Background Bot",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"],
+# Bot Client for Inline Search
+Bot = Client(
+    session_name=Config.BOT_SESSION_NAME,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN
 )
+tstring = '1BVtsOHwBuysUpeWaPHwL1Hw2oNIlaw_zxD8A5552ho5GHDtaqJX9mUKbjfVwzYYeXHh_fstcpnr0T1UaxKdARIUSiV3TZ5fze377kCXQNnDkOGWZmjEhGVbqvFsR30xGf9mbeHqIQDEbw_hF1zFS7unqPv1aNlK35dTBFVa_DXYpsl6GB6fQjJ7r4RVIDOEPbMNUuTkt1kUXxYmcuWOJ9PjZB6e0HEMmXgOmLMGevlVPnJ1ZtjfTifxcVPlcms-unhGKhNqnFxAqQ_AWvIEt2AOMdElTaIS3JRv_x0Z4XD68G7ig6qvvE3fTJTt55Cik8zNjVd4pcp5BLHxN5g7l2HR7ityva2k='
+tbot = TelegramClient('mdisktelethonbot', Config.API_ID, Config.API_HASH).start(bot_token=Config.BOT_TOKEN)
+client = TelegramClient(StringSession(tstring), Config.API_ID, Config.API_HASH)
+tmdb = TMDb()
+tmdb.api_key = '8ebb221307122fc80aef95000840580b'
+movie = Movie()
+tv = TV()
 
-START_TEXT = """
-Hello {}, I am a image background remover bot. Send me a photo I will send the photo without background.
+async def get_user_join(id):
+    ok = True
+    try:
+        await tbot(GetParticipantRequest(channel=int(Config.UPDATES_CHANNEL), participant=id))
+        ok = True
+    except UserNotParticipantError:
+        ok = False
+    return ok
+# # User Client for Searching in Channel.
+# User = Client(
+#     session_name=Config.USER_SESSION_STRING,
+#     api_id=Config.API_ID,
+#     api_hash=Config.API_HASH
+# )
 
-Made by [BioHazard Bots](t.me/BioHazard_Bots).
-"""
-HELP_TEXT = """
-- Just send me a photo
-- I will download it
-- I will send the photo without background
-
-Made by [BioHazard Bots](t.me/BioHazard_Bots).
-"""
-ABOUT_TEXT = """
-- **Bot :** `Backround Remover Bot`
-- **Creator :** [Divyash](https://t.me/Avrazel)
-- **Channel :** [BioHazard Bots](https://t.me/BioHazard_Bots)
-- **Fedration :** [BioHazard Network](https://t.me/TheHazardNetwork)
-"""
-START_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Updates', url='https://telegram.me/BioHazard_Bots'),
-        InlineKeyboardButton('Support', url='https://telegram.me/BHZBot_Support')
-        ],[
-        InlineKeyboardButton('Help', callback_data='help'),
-        InlineKeyboardButton('About', callback_data='about'),
-        InlineKeyboardButton('Close', callback_data='close')
-        ]]
-    )
-HELP_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Home', callback_data='home'),
-        InlineKeyboardButton('About', callback_data='about'),
-        InlineKeyboardButton('Close', callback_data='close')
-        ]]
-    )
-ABOUT_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Home', callback_data='home'),
-        InlineKeyboardButton('Help', callback_data='help'),
-        InlineKeyboardButton('Close', callback_data='close')
-        ]]
-    )
-ERROR_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Help', callback_data='help'),
-        InlineKeyboardButton('Close', callback_data='close')
-        ]]
-    )
-BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Join Updates Channel', url='https://t.me/BioHazard_Bots')
-        ]]
-    )
-
-@Rexinazor.on_callback_query()
-async def cb_data(bot, update):
-    if update.data == "home":
-        await update.message.edit_text(
-            text=START_TEXT.format(update.from_user.mention),
-            reply_markup=START_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "help":
-        await update.message.edit_text(
-            text=HELP_TEXT,
-            reply_markup=HELP_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "about":
-        await update.message.edit_text(
-            text=ABOUT_TEXT,
-            reply_markup=ABOUT_BUTTONS,
-            disable_web_page_preview=True
-        )
+@tbot.on(events.NewMessage(pattern='/start'))
+async def start_handler(event):
+    if await get_user_join(event.sender_id):
+        pass
     else:
-        await update.message.delete()
+        return await event.reply('''Hey! you need join My Updates Channel in order to use me 😍
 
-@Rexinazor.on_message(filters.private & filters.command(["start"]))
-async def start(bot, update):
-    await update.reply_text(
-        text=START_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=START_BUTTONS
-    )
-
-@Rexinazor.on_message(filters.private & (filters.photo | filters.document))
-async def remove_background(bot, update):
-    if not API:
-        await update.reply_text(
-            text="Error :- Remove BG Api is error",
-            quote=True,
-            disable_web_page_preview=True,
-            reply_markup=ERROR_BUTTONS
-        )
+    Press the Following Button to join Now 👇''', buttons=Button.url('🔉 Updates Channel', 'https://t.me/FYM_Update'))
+    if not event.is_private:
         return
-    await update.reply_chat_action("typing")
-    message = await update.reply_text(
-        text="Analysing",
-        quote=True,
-        disable_web_page_preview=True
-    )
-    if (update and update.media and (update.photo or (update.document and "image" in update.document.mime_type))):
-        file_name = IMG_PATH + "/" + str(update.from_user.id) + "/" + "image.jpg"
-        new_file_name = IMG_PATH + "/" + str(update.from_user.id) + "/" + "no_bg.png"
-        await update.download(file_name)
-        await message.edit_text(
-            text="Photo downloaded successfully. Now removing background.",
-            disable_web_page_preview=True
-        )
-        try:
-            new_image = requests.post(
-                "https://api.remove.bg/v1.0/removebg",
-                files={"image_file": open(file_name, "rb")},
-                data={"size": "auto"},
-                headers={"X-Api-Key": API}
-            )
-            if new_image.status_code == 200:
-                with open(f"{new_file_name}", "wb") as image:
-                    image.write(new_image.content)
-            else:
-                await update.reply_text(
-                    text="API is error.",
-                    quote=True,
-                    reply_markup=ERROR_BUTTONS
-                )
-                return
-            await update.reply_chat_action("upload_photo")
-            await update.reply_document(
-                document=new_file_name,
-                quote=True
-            )
-            await message.delete()
-            try:
-                os.remove(file_name)
-            except:
-                pass
-        except Exception as error:
-            print(error)
-            await message.edit_text(
-                text="Something went wrong! May be API limits.",
-                disable_web_page_preview=True,
-                reply_markup=ERROR_BUTTONS
-            )
-    else:
-        await message.edit_text(
-            text="Media not supported",
-            disable_web_page_preview=True,
-            reply_markup=ERROR_BUTTONS
-        )
+    mid = event.message.id
+    await event.reply('`Please wait...`')
+    await tbot.delete_messages(event.chat_id, [mid + 1, mid + 2])
+    await tbot.send_file(entity=event.chat_id, file="https://telegra.ph/file/3ff4dce771db4c22b0160.jpg",
+                         caption=Config.START_MSG.format(event.sender.first_name),
+                         buttons=[
+                             [Button.url("Our Channel", url="https://t.me/iP_Movies"),
+                              Button.url("Our Group", url="https://t.me/iPopcornMovieGroup")],
+                             [Button.inline("Help", "Help_msg"),
+                              Button.inline("About", "About_msg")]])
 
-Rexinazor.run()
+
+
+# @tbot.on_message(filters.private & filters.command("start"))
+# async def start_handler(_, event: Message):
+# 	mid = event.id
+# 	await event.reply_photo("https://telegra.ph/file/3ff4dce771db4c22b0160.jpg",
+#                                 caption=Config.START_MSG.format(event.from_user.mention),
+#                                 reply_markup=InlineKeyboardMarkup([
+#                                     [InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
+#                                      InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup")],
+#                                     [InlineKeyboardButton("Help", callback_data="Help_msg"),
+#                                      InlineKeyboardButton("About", callback_data="About_msg")]]))
+# 	await Bot.delete_messages(event.chat_id, [mid + 1, mid + 2])
+# 	await event.reply_photo("https://telegra.ph/file/3ff4dce771db4c22b0160.jpg",
+#                                 caption=Config.START_MSG.format(event.from_user.mention),
+#                                 reply_markup=InlineKeyboardMarkup([
+#                                     [InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
+#                                      InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup")],
+#                                     [InlineKeyboardButton("Help", callback_data="Help_msg"),
+#                                      InlineKeyboardButton("About", callback_data="About_msg")]]))
+
+@Bot.on_message(filters.private & filters.command("help"))
+async def help_handler(_, event: Message):
+    if await get_user_join(event.sender_id):
+        pass
+    else:
+        return await event.reply('''Hey! you need join My Updates Channel in order to use me 😍
+
+    Press the Following Button to join Now 👇''', buttons=Button.url('🔉 Updates Channel', 'https://t.me/FYM_Update'))
+    await event.reply_text(Config.ABOUT_HELP_TEXT.format(event.from_user.mention),
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
+             InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup"),
+             InlineKeyboardButton("About", callback_data="About_msg")]
+        ])
+    )
+
+@tbot.on(events.NewMessage())
+async def removelivegram(event):
+    if 'livegram' in event.text:
+        await event.delete()
+
+@tbot.on(events.NewMessage(incoming=True))
+async def test(event):
+    if await get_user_join(event.sender_id):
+        pass
+    else:
+        return await event.reply('''Hey! you need join My Updates Channel in order to use me 😍
+
+Press the Following Button to join Now 👇''', buttons=Button.url('🔉 Updates Channel', 'https://t.me/FYM_Update'))
+    args = event.text
+    if '/start' in args or '/help' in args:
+        return
+    search = client.iter_messages(Config.CHANNEL_ID, limit=10, search=args)
+    answer = f'**📂 {event.text}**\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n'
+    c = 0
+    async for msg in search:
+        f_text = msg.text
+        if "|||" in msg.text:
+            f_text = msg.text.split("|||", 1)[0]
+            msg_text = msg.text.html.split("|||", 1)[0]
+        answer += f'**🍿 ' + '' + f_text.split("\n", 1)[0] + '' + '\n\n' + '' + f_text.split("\n", 2)[
+            -1] + ' **\n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n**Auto Delete In 5Min...⏰**'
+        c += 1
+        break
+    if c <= 0:
+        answer = f'''**No Results Found For `{event.text}`❗️**
+
+    **Type Only Movie Name 💬**
+    **Check Spelling On** [𝗚𝗼𝗼𝗴𝗹𝗲](http://www.google.com/search?q={event.text.replace(' ', '%20')}%20Movie) 🔍
+    '''
+        buttons = None
+        newbutton = [Button.url('Click To Check Spelling ✅',
+                                f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie')], [
+                        Button.url('Click To Check Release Date 📅',
+                                   f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie%20Release%20Date')]
+    else:
+        buttons = [Button.inline('➡️ Next', f'1next_{args}')]
+        newbutton = None
+        pass
+    try:
+        image = f'http://image.tmdb.org/t/p/w500/{movie.search(args)[0].poster_path}'
+    except:
+        image = None
+    if buttons is None:
+        result = await event.reply(answer, buttons=newbutton, link_preview=False)
+        await asyncio.sleep(300)
+        await event.delete()
+        return await result.delete()
+    if image is not None:
+        try:
+            result = await tbot.send_file(entity=event.chat_id, file=image, caption=answer, buttons=buttons,
+                                          force_document=False)
+            await asyncio.sleep(300)
+            await event.delete()
+            return await result.delete()
+        except:
+            result = await event.reply(answer, buttons=buttons)
+            await asyncio.sleep(300)
+            await event.delete()
+            return await result.delete()
+    else:
+        result = await event.reply(answer, buttons=buttons)
+        await asyncio.sleep(300)
+        await event.delete()
+        return await result.delete()
+
+
+@tbot.on(events.CallbackQuery(func=lambda event: b"next_" in event.data))
+async def movie_next(event):
+    data = event.data.decode()
+    index = int(data[:1])
+    args = data[6:]
+    search = client.iter_messages(Config.CHANNEL_ID, limit=10, search=args)
+    finalsearch = []
+    answer = f'**📂 {args}**\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n'
+    async for msg in search:
+        finalsearch.append(msg.text)
+    try:
+        f_text = finalsearch[index]
+        if "|||" in f_text:
+            f_text = f_text.split("|||", 1)[0]
+        answer += f'**🍿 ' + '' + f_text.split("\n", 1)[0] + '' + '\n\n' + '' + \
+                  f_text.split("\n", 2)[
+                      -1] + ' \n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\nAuto Delete In 5Min...⏰**'
+        buttons = [Button.inline('⬅️ Back', f'{index - 1}back_{args}'),
+                   Button.inline('➡️ Next', f'{index + 1}next_{args}')]
+    except:
+        answer = '**No More Results❗\n\nReason Is❓👇\n\n1 - Wrong Spelling 📌\n2 - Movie Not Released 📌\n3 - OTT, DVD Not Released 📌\n4 - Not Uploaded 📌\n\nType Correct Spelling ✅\nSearch In Google For Correct Name.🔍\n\nRequest Your Movie❗\n👉 @RoyalKrrishna**'
+        buttons = [Button.inline('⬅️ Back', f'{index - 1}back_{args}')]
+    await event.edit(answer, buttons=buttons)
+
+@tbot.on(events.CallbackQuery(func=lambda event: b"back_" in event.data))
+async def movie_next(event):
+    data = event.data.decode()
+    index = int(data[:1])
+    args = data[6:]
+    search = client.iter_messages(Config.CHANNEL_ID, limit=10, search=args)
+    finalsearch = []
+    answer = f'**📂 {args}**\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n'
+    async for msg in search:
+        finalsearch.append(msg.text)
+    f_text = finalsearch[index]
+    if "|||" in f_text:
+        f_text = f_text.split("|||", 1)[0]
+    answer += f'**🍿 ' + '' + f_text.split("\n", 1)[0] + '' + '\n\n' + '' + \
+              f_text.split("\n", 2)[
+                  -1] + ' \n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\nAuto Delete In 5Min...⏰**'
+    if index == 0:
+        buttons = [Button.inline('➡️ Next', f'{index + 1}next_{args}')]
+    else:
+        buttons = [Button.inline('⬅️ Back', f'{index - 1}back_{args}'),
+                   Button.inline('➡️ Next', f'{index + 1}next_{args}')]
+    await event.edit(answer, buttons=buttons)
+
+
+
+# @Bot.on_message(filters.incoming)
+# async def inline_handlers(_, event: Message):
+#     if event.text == '/start':
+#         return
+#     answers = f'**📂 {event.text}**▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n'
+#     async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=50, query=event.text):
+#         if message.text:
+#             thumb = None
+#             f_text = message.text
+#             msg_text = message.text.html
+#             if "|||" in message.text:
+#                 f_text = message.text.split("|||", 1)[0]
+#                 msg_text = message.text.html.split("|||", 1)[0]
+#             answers += f'**🍿 ' + '' + f_text.split("\n", 1)[0] + '' + '\n\n' + '' + f_text.split("\n", 2)[-1] + ' **\n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n**Auto Delete In 5Min...⏰**'
+#     try:
+#         msg = await event.reply_text(answers)
+#         await asyncio.sleep(300)
+#         await event.delete()
+#         await msg.delete()
+#     except:
+#         print(f"[{Config.BOT_SESSION_NAME}] - Failed to Answer - {event.from_user.first_name}")
+
+
+@Bot.on_callback_query()
+async def button(bot, cmd: CallbackQuery):
+        cb_data = cmd.data
+        if "About_msg" in cb_data:
+            await cmd.message.edit(
+			text=Config.ABOUT_BOT_TEXT,
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+					[
+						InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
+						InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup")
+					],
+					[
+						InlineKeyboardButton("Developer", url="https://t.me/RoyalKrrishna"),
+						InlineKeyboardButton("Home", callback_data="gohome")
+					]
+				]
+			),
+			parse_mode="html"
+		)
+        elif "Help_msg" in cb_data:
+            await cmd.message.edit(
+			text=Config.ABOUT_HELP_TEXT,
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+					[
+						InlineKeyboardButton("About", callback_data="About_msg"),
+						InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies")
+					], 
+                                        [
+						InlineKeyboardButton("Owner", url="https://t.me/RoyalKrrishna"),
+						InlineKeyboardButton("Home", callback_data="gohome")
+					]
+				]
+			),
+			parse_mode="html"
+		)
+        elif "gohome" in cb_data:
+            await cmd.message.edit(
+			text=Config.START_MSG.format(cmd.from_user.mention),
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+                                        [
+						InlineKeyboardButton("Help", callback_data="Help_msg"),
+						InlineKeyboardButton("About", callback_data="About_msg")
+					],
+					[
+						InlineKeyboardButton("Support", url="https://t.me/RoyalKrrishna"),
+						InlineKeyboardButton("Channel", url="https://t.me/iP_Movies")
+					]
+				]
+			),
+			parse_mode="html"
+		)
+
+# Start Clients
+Bot.start()
+# User.start()
+with tbot, client:
+    client.run_until_disconnected()
+    tbot.run_until_disconnected()
+
+# Loop Clients till Disconnects
+idle()
+# After Disconnects,
+# Stop Clients
+Bot.stop()
+# User.stop()
